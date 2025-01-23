2714941bec543034a563760a0e5e35840569460f
hojae <ghwo336@naver.com>
2025-01-23 20:29:38 +0900

    feat: 중간 승인

--- src/pages/ForgotPage.tsx
+++ src/pages/ForgotPage.tsx
@@ -1,16 +1,35 @@
 import styled from "@emotion/styled";
 import LoginHeader from "../widgets/LoginHeader";
 import AcountContainer from "../widgets/AcountContainer";
+import {keyframes} from "@emotion/react";
+interface AnimatedContainerProps {
+  delay?: string;
+}
+
+const fadeIn = keyframes`
+    from {
+      opacity:0;
+      transform: translateY(-50px);
+    }
+    to{
+      opacity:1;
+      transform:translateY(0);
+    }
+  `;
+
+const AnimatedContainer = styled.div<AnimatedContainerProps>`
+  opacity: 0;
+  animation: ${fadeIn} 1s ease-in-out forwards;
+  animation-delay: ${(props) => props.delay || "0s"};
+`;
 
 const Wrapper = styled.div`
-  max-width: 1440px;
-  height: 100vh; /* 전체 화면 높이 */
-  display: flex;
-  flex-direction: column; /* 세로 방향 정렬 */
+  width: 100%;
 `;
 
 const HeaderWrapper = styled.div`
   flex-shrink: 0; /* Header 크기를 유지 */
+  margin-bottom: 38px;
 `;
 
 const ContentWrapper = styled.div`
@@ -18,21 +37,34 @@ const ContentWrapper = styled.div`
   display: flex;
   align-items: center; /* 가로 중앙 정렬 */
   justify-content: center; /* 세로 중앙 정렬 */
+  margin-top: 120px;
+`;
+
+const LayOut = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 800px;
+  gap: 133px;
 `;
 
 const ForgotPage = () => {
   return (
-    <Wrapper>
-      <HeaderWrapper>
-        <LoginHeader />
-      </HeaderWrapper>
-      <ContentWrapper>
-        <AcountContainer
-          largeLetter="Forgot Password?"
-          smallLetter="Quick & Simple way to find your password"
-        />
-      </ContentWrapper>
-    </Wrapper>
+    <LayOut>
+      <Wrapper>
+        <HeaderWrapper>
+          <LoginHeader />
+        </HeaderWrapper>
+        <AnimatedContainer delay="0.5s">
+          <ContentWrapper>
+            <AcountContainer
+              largeLetter="Forgot Password?"
+              smallLetter="Quick & Simple way to find your password"
+            />
+          </ContentWrapper>
+        </AnimatedContainer>
+      </Wrapper>
+    </LayOut>
   );
 };
 
